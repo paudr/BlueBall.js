3542cd349f5e39e88017bde9332c56b4b88b44d2
pau.drudis <none@none>
2014-06-27 11:53:59 +0200

    Se crea una habitacion vacia

--- js/src/game.js
+++ js/src/game.js
@@ -6,9 +6,26 @@ jQuery(function () {
 
     game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameLayer', {
 
-        'preload': function () {},
+        'preload': function () {
 
-        'create': function () {},
+            game.load.image('AdventuresOfLolo3', 'assets/tilemaps/tiles/AdventuresOfLolo3.png');
+            game.load.tilemap('blank', 'assets/tilemaps/maps/blank.json', null, Phaser.Tilemap.TILED_JSON);
+
+        },
+
+        'create': function () {
+
+            var map = game.add.tilemap('blank'),
+                layers = game.add.group();
+
+            layers.x = 50;
+            layers.y = 50;
+
+            map.addTilesetImage('AdventuresOfLolo3', 'AdventuresOfLolo3');
+
+            map.createLayer('environment', undefined, undefined, layers);
+
+        },
 
         'update': function () {}
 
