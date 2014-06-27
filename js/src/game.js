6a01a143d1fc03572f0ec9f1f016a2f17bc67961
pau.drudis <none@none>
2014-06-27 12:08:39 +0200

    Se crea una sprite de lolo

--- js/src/game.js
+++ js/src/game.js
@@ -2,7 +2,9 @@
 
 jQuery(function () {
 
-    var game;
+    var game,
+        cursors,
+        smallLolo;
 
     game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameLayer', {
 
@@ -11,12 +13,15 @@ jQuery(function () {
             game.load.image('AdventuresOfLolo3', 'assets/tilemaps/tiles/AdventuresOfLolo3.png');
             game.load.tilemap('blank', 'assets/tilemaps/maps/blank.json', null, Phaser.Tilemap.TILED_JSON);
 
+            game.load.atlas('smallLolo', 'assets/sprites/smallLolo.png', 'assets/sprites/smallLolo.json');
+
         },
 
         'create': function () {
 
             var map = game.add.tilemap('blank'),
-                layers = game.add.group();
+                layers = game.add.group(),
+                players = game.add.group(layers);
 
             layers.x = 50;
             layers.y = 50;
@@ -25,9 +30,45 @@ jQuery(function () {
 
             map.createLayer('environment', undefined, undefined, layers);
 
+            cursors = game.input.keyboard.createCursorKeys();
+            smallLolo = game.add.sprite(32 * 1, 32 * 6, 'smallLolo', 10, players);
+            smallLolo.animations.add('Top', Phaser.Animation.generateFrameNames('loloTop', 0, 4, '', 4), 5, true);
+            smallLolo.animations.add('Right', Phaser.Animation.generateFrameNames('loloRight', 0, 4, '', 4), 5, true);
+            smallLolo.animations.add('Down', Phaser.Animation.generateFrameNames('loloDown', 0, 4, '', 4), 5, true);
+            smallLolo.animations.add('Left', Phaser.Animation.generateFrameNames('loloLeft', 0, 4, '', 4), 5, true);
+
+            smallLolo.scale.set(32 / 17);
+
+            layers.bringToTop(players);
+
         },
 
-        'update': function () {}
+        'update': function () {
+
+            if (cursors.up.isDown) {
+
+                smallLolo.animations.play('Top');
+
+            } else if (cursors.down.isDown) {
+
+                smallLolo.animations.play('Down');
+
+            } else if (cursors.left.isDown) {
+
+                smallLolo.animations.play('Left');
+
+            } else if (cursors.right.isDown) {
+
+                smallLolo.animations.play('Right');
+
+            } else {
+
+                smallLolo.animations.stop();
+                smallLolo.frame = 10;
+
+            }
+
+        }
 
     });
 
