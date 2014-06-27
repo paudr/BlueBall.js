732db4760757c43586c3868df08bebb1931371c9
pau.drudis <none@none>
2014-06-27 11:44:49 +0200

    Proyecto vacio

---
+++ js/src/game.js
@@ -0,0 +1,17 @@
+/*global jQuery, Phaser */
+
+jQuery(function () {
+
+    var game;
+
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameLayer', {
+
+        'preload': function () {},
+
+        'create': function () {},
+
+        'update': function () {}
+
+    });
+
+});
