890ea0aae6735ce8d7a9b6cabd4c151be90bc07f
pau.drudis <none@none>
2014-07-08 11:26:20 +0200

    Se añade un tercer mapa

--- js/src/game.js
+++ js/src/game.js
@@ -8,6 +8,7 @@ jQuery(function () {
     game.state.add('boot', new BlueBall.Boot());
     game.state.add('level1-1', new BlueBall.Level('level1-1'));
     game.state.add('level1-2', new BlueBall.Level('level1-2'));
+    game.state.add('level1-3', new BlueBall.Level('level1-3'));
 
     game.state.start('boot');
 
