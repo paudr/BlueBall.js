f9820f0530604ced3a54c3e4b52c2646c322f468
Pau Drudis <frikeldon@gmail.com>
2015-02-22 18:49:03 +0100

    Se elimina codigo antiguo

--- src/entities/projectiles/projectileEgg.js
+++ src/entities/projectiles/projectileEgg.js
@@ -6,9 +6,6 @@ BlueBall.ProjectileEgg = function (shooter, direction) {
 
     BlueBall.Projectile.call(this, shooter, direction, 'eggSprites', frame);
 
-    this.collideIndexes.push(2, 69, 81, 93, 97, 100);
-    this.impactIndexes.push(69, 81, 93, 97, 100);
-
 };
 
 BlueBall.ProjectileEgg.prototype = Object.create(BlueBall.Projectile.prototype);
