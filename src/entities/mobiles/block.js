417fab402801a488728162c3cbe370068e6bf4dc
Pau Drudis <frikeldon@gmail.com>
2016-01-28 20:47:28 +0100

    Block utiliza el mecanismo de tiles arrow

--- src/entities/mobiles/block.js
+++ src/entities/mobiles/block.js
@@ -5,3 +5,6 @@ BlueBall.Block = function (game, x, y, key, frame) {
 };
 
 BlueBall.Block.prototype = Object.create(BlueBall.Mobile.prototype);
+
+BlueBall.Block.prototype.tilesThatCollide = BlueBall.Helper.getTileIds('Rock', 'Bush', 'Lava', 'Wall', 'Water', 'Bridge', 'LavaBridge', 'Grass');
+BlueBall.Block.prototype.tilesThatArrow = BlueBall.Helper.getTileIds('Arrow');
